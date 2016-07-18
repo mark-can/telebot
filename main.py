2d8978fd63f3b14daba9fbd44f037cdc8ab3e213
mark-can <kandaurov.mark@gmail.com>
2016-07-19 00:22:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-TOKEN = 'YOUR_BOT_TOKEN_HERE'
+TOKEN = '185396993:AAFA62dJDLiyw6AZGYT_bMmt8SqQIK_b1Nw'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
